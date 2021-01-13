c6a73e1dc04ee662605e61ac39fb0e2a8b8810a0
Noah Demi <58009902+NoahDemi@users.noreply.github.com>
2021-01-13 11:35:18 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+import Header from "./components/common/Header";
+import PageBody from "./components/common/PageBody";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
