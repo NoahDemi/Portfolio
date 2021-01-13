20361b358725f6a4fcbc76d07ff38ea35d640399
Noah Demi <58009902+NoahDemi@users.noreply.github.com>
2021-01-13 14:25:35 -0500

    Success! Created a nav side bar that goes page to page!

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import Contact from "./components/Pages/Contact";
+import Projects from "./components/Pages/Projects";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -11,7 +15,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" />
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+
+          <Route path="/Contact" exact component={Contact} />
         </Switch>
       </Router>
     </>
